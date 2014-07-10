7d11d06409cc9975fdaf94184eabf161dfea94f1
Matthew Patterson <matthew.s.patterson@gmail.com>
2014-07-10 15:58:33 -0500

    Add cssmin; start work on websockets

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,12 +6,33 @@ module.exports = function(grunt) {
       options: {
         separator: ';'
       },
-      all: {
+      "javascript": {
         src: [
           "bower_components/jquery/dist/jquery.min.js",
-          "tmp/_uglified_javascript/**/*.js"
+          "tmp/_minified_javascript/**/*.js"
         ],
         dest: "dist/static/<%= pkg.name %>.js"
+      },
+      "javascript:dev": {
+        src: [
+          "bower_components/jquery/dist/jquery.min.js",
+          "src/static/**/*.js"
+        ],
+        dest: "dist/static/<%= pkg.name %>.js"
+      },
+      "stylesheets": {
+        src: [
+          "bower_components/pure/*-min.css",
+          "tmp/_minified_stylesheets/**/*.css"
+        ],
+        dest: "dist/static/<%= pkg.name %>.css"
+      },
+      "stylesheets:dev": {
+        src: [
+          "bower_components/pure/*-min.css",
+          "src/static/**/*.css"
+        ],
+        dest: "dist/static/<%= pkg.name %>.css"
       }
     },
 
@@ -19,31 +40,35 @@ module.exports = function(grunt) {
       options: {
         banner: "/*! Compiled at <%= grunt.template.today(\"yyyy-mm-dd HH:MM:ss\") %> */\n",
       },
-      package: {
+      "javascript": {
         files: [{
           expand: true,
           src: "src/static/**/*.js",
-          dest: "tmp/_uglified_javascript"
+          dest: "tmp/_minified_javascript"
         }]
-      },
-      package_dev: {
-        options: {
-          mangle: false,
-          compress: false,
-          beautify: true,
-        },
-        files: [{
-          expand: true,
-          src: "src/static/**/*.js",
-          dest: "tmp/_uglified_javascript"
-        }]
+      }
+    },
+
+    cssmin: {
+      minify: {
+        expand: true,
+        src: ["src/static/**/*.css"],
+        dest: "tmp/_minified_stylesheets",
+        ext: ".min.css"
       }
     }
   });
 
   grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
   grunt.loadNpmTasks("grunt-contrib-uglify");
 
-  grunt.registerTask("assets:install", ["uglify:package", "concat:all"]);
-  grunt.registerTask("assets:install:dev", ["uglify:package_dev", "concat:all"]);
+  grunt.registerTask("assets:javascript:install", ["uglify:javascript", "concat:javascript"]);
+  grunt.registerTask("assets:javascript:install:dev", ["concat:javascript:dev"]);
+
+  grunt.registerTask("assets:stylesheets:install", ["cssmin:minify", "concat:stylesheets"]);
+  grunt.registerTask("assets:stylesheets:install:dev", ["concat:stylesheets:dev"]);
+
+  grunt.registerTask("assets:install", ["assets:javascript:install", "assets:stylesheets:install"]);
+  grunt.registerTask("assets:install:dev", ["assets:javascript:install:dev", "assets:stylesheets:install:dev"]);
 };
