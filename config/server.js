ec181bdf6ff0e2f0792195c663109319b1065f29
Matthew Patterson <matthew.s.patterson@gmail.com>
2015-01-23 15:24:42 -0600

    Remove numWorkers from configuration

--- config/server.js
+++ config/server.js
@@ -1,8 +1,6 @@
 'use strict';
-var numCPU = require('os').cpus().length;
 
 module.exports = {
   baseUrl: process.env.BASE_URL || 'http://localhost:8000',
-  numWorkers: process.env.NUM_WORKERS || numCPU,
   port: process.env.PORT || 8000
 };
